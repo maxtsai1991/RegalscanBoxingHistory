1a9f9569587ab5552d222d61fc79e717d48e6c62
MaxTsai1991 <s860473@gmail.com>
2022-06-07 11:08:40 +0800

    1. 兩個Activity 元件綁定 及 監聽器設定 及 跳轉

--- app/src/main/java/max/com/regalscanboxinghistory/view/BoxingActivity.java
+++ app/src/main/java/max/com/regalscanboxinghistory/view/BoxingActivity.java
@@ -2,15 +2,131 @@ package max.com.regalscanboxinghistory.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import max.com.regalscanboxinghistory.R;
 
+/**
+ * 筆記 :
+ *  1.   View 畫面 和 元件 處理
+ */
 public class BoxingActivity extends AppCompatActivity {
+    /**
+     * 返回主選單 , 進入裝箱作業-工作歷程(歷程記錄)
+     */
+    private ImageView iv_back_main, iv_to_history;
+    private String vendorName = "帝商";
+    /**
+     * 供應商名稱
+     */
+    private TextView tv_vendor_boxing;
+    /**
+     * 條碼資訊(號碼) , 數量
+     */
+    private EditText et_num, et_quantity;
+    /**
+     * CheckBox(手動/自動)
+     */
+    private CheckBox cb_no_auto;
+    /**
+     * 列印
+     */
+    private Button bt_print;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boxing);
+        findViews();
+        handleClickListener();
+
     }
+
+    private void findViews() {
+        // 返回主選單
+        iv_back_main = findViewById(R.id.iv_back_main);
+        // 進入裝箱作業-工作歷程(歷程記錄)
+        iv_to_history = findViewById(R.id.iv_to_history);
+        // 條碼資訊(號碼)
+        et_num = findViewById(R.id.et_num);
+        // 數量
+        et_quantity = findViewById(R.id.et_quantity);
+        // 手動
+        cb_no_auto = (CheckBox) findViewById(R.id.cb_no_auto);
+        // 列印
+        bt_print = findViewById(R.id.bt_print);
+        // 供應商
+        tv_vendor_boxing = findViewById(R.id.tv_vendor_boxing);
+    }
+
+    private void handleClickListener() {
+        /**
+         * 跳轉下一頁(歷程記錄)
+         */
+        iv_to_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BoxingActivity.this, HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /**
+         * 列印 : 暫時先取得三種資料 : 供應商 號碼 數量
+         */
+        bt_print.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        /**
+         *  et_num EditText【號碼】監聽事件
+         */
+        et_num.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+
+                return false; //回傳 false 表示到這邊結束，回傳 true 則會繼續原本 onKey 定義的動作。
+            }
+        });
+
+        /**
+         *  et_quantity EditText【數量】監聽事件
+         */
+        et_quantity.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                return false; //回傳 false 表示到這邊結束，回傳 true 則會繼續原本 onKey 定義的動作。
+            }
+        });
+
+        /**
+         * Checkbox(手動/自動) 自動(isChecked = false)
+         */
+        cb_no_auto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+
+
+
+    }
+
 }
--- app/src/main/java/max/com/regalscanboxinghistory/view/HistoryActivity.java
+++ app/src/main/java/max/com/regalscanboxinghistory/view/HistoryActivity.java
@@ -1,16 +1,56 @@
 package max.com.regalscanboxinghistory.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import max.com.regalscanboxinghistory.R;
 
 public class HistoryActivity extends AppCompatActivity {
+    /**
+     * 返回到裝箱作業
+     */
+    private View iv_return_boxing;
+    /**
+     * 供應商
+     */
+    private TextView tv_vendor_name_history;
+    /**
+     * Recyclerview id
+     */
+    private RecyclerView rv_info_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        findViews();
+        handleClickListener();
     }
+
+    private void findViews() {
+        iv_return_boxing = findViewById(R.id.iv_return_boxing);
+        tv_vendor_name_history = findViewById(R.id.tv_vendor_name_history);
+        rv_info_list = findViewById(R.id.rv_info_list);
+    }
+
+    private void handleClickListener() {
+        /**
+         * 返回到裝箱作業
+         */
+        iv_return_boxing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HistoryActivity.this, BoxingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
 }
